54a255b6128bbfc5ca2586006286d5f45370ff76
mimu <akito.mimura@nijibox.co.jp>
2023-06-07 18:13:53 +0900

    commit

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,9 +1,9 @@
-import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterSlice";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 
 const Counter = () => {
-  const count = useSelector((state: any) => state.counter.value);
-  const dispatch = useDispatch();
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
   return (
     <div>
       <div>
